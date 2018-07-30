76e5a01c15ed7559cc8dd62a70f41ec0efaf50fa
dukup11ch1 <dukup11ch1@gmail.com>
2018-07-30 17:08:01 +0900

    print add

--- gmail_login.py
+++ gmail_login.py
@@ -3,4 +3,5 @@ import imaplib
 def gmail_login(username, password):
     IMAP_SERVER = imaplib.IMAP4_SSL(str(socket.gethostbyname('imap.gmail.com')),993)
     IMAP_SERVER.login(username, password)
+    print 'login success'
     return IMAP_SERVER
